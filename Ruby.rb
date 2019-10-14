42ebe612301ea65db94328162c1bceead093cdb5
Tatyana-Plus <56022860+Tatyana-Plus@users.noreply.github.com>
2019-10-15 00:17:45 +0300

    Add files via upload

---
+++ Ruby.rb
@@ -0,0 +1,65 @@
+class Polygon
+  attr_reader :side_length, :number_of_sides
+
+  def initialize(name, side_length, number_of_sides)
+    @side_length = side_length.to_i
+    @number_of_sides = number_of_sides.to_i
+    super name
+  end
+  
+  def calculate_perimiter
+    @side_length.to_i * @number_of_sides
+  end
+  
+  def calculate_apothem
+    @side_length / (2 *  Math.tan(3.14159/@number_of_sides))
+  end
+  
+   def calculate_area 
+ (calculate_perimiter * calculate_apothem)/2
+  end
+  
+  def to_string
+ shape_string("side length", @side_length.to_s, calculate_perimiter.round(2).to_s, calculate_area.round(2).to_s)
+  end
+end
+  
+  class Triangle < Polygon
+
+  def initialize(side_length)
+    super("triangle", side_length, 3)
+  end
+end
+tr = Triangle.newclass Polygon
+  attr_reader :side_length, :number_of_sides
+
+  def initialize(name, side_length, number_of_sides)
+    @side_length = side_length.to_i
+    @number_of_sides = number_of_sides.to_i
+    super name
+  end
+  
+  def calculate_perimiter
+    @side_length.to_i * @number_of_sides
+  end
+  
+  def calculate_apothem
+    @side_length / (2 *  Math.tan(3.14159/@number_of_sides))
+  end
+  
+   def calculate_area 
+ (calculate_perimiter * calculate_apothem)/2
+  end
+  
+  def to_string
+ shape_string("side length", @side_length.to_s, calculate_perimiter.round(2).to_s, calculate_area.round(2).to_s)
+  end
+end
+  
+  class Triangle < Polygon
+
+  def initialize(side_length)
+    super("triangle", side_length, 3)
+  end
+end
+tr = Triangle.new(side_length)
